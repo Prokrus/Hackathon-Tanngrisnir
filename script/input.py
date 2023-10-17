f96c694a0f878d13fe0ecd0b29cfbebc24048cd6
wings0728 <36744020@qq.com>
2023-10-18 01:46:29 +0800

    finial version

--- script/input.py
+++ script/input.py
@@ -9,11 +9,11 @@ import random
 import queue
 
 VIDEO_PATH = '../../Driving_in_London.mp4'
-TIME_FEATURE_0 = 1 # sec
-TIME_FEATURE_1 = 2 # sec
-TIME_FEATURE_2 = 3 # sec
-TIME_FEATURE_3 = 4 # sec
-TIME_FEATURE_4 = 5 # sec
+TIME_FEATURE_0 = 10 # sec features inquire
+TIME_FEATURE_1 = 20 # sec high incar_temperature
+TIME_FEATURE_2 = 45 # sec quiet vehicle & local life suggestion
+TIME_FEATURE_3 = 80 # sec traffic light Red-to-green reminder
+TIME_FEATURE_4 = 85 # sec Fatigue Relief
 
 queue = queue.Queue()
 # video play time to get feature
@@ -78,29 +78,109 @@ def monitor_time():
         print("Currently Video Time：{:.1f} Sec".format(play_time))
         time.sleep(1)
 
-        if int(play_time) == TIME_FEATURE_0:
+        if int(play_time) >= TIME_FEATURE_0 and int(play_time) < TIME_FEATURE_1:
             print("feature0")
             # doing something
+            data.setData(1,
+                         random.randint(23, 25),
+                         True,
+                         True,
+                         random.randint(1, 3),
+                         False,
+                         False,
+                         random.random() < 0.5,
+                         random.random(),
+                         random.randint(80, 81),
+                         2,
+                         True,
+                         True,
+                         random.randint(30, 35),
+                         False,
+                         False)
 
-        elif int(play_time) == TIME_FEATURE_1:
+        elif int(play_time) >= TIME_FEATURE_0 and int(play_time) < TIME_FEATURE_1:
             print("feature1")
             # doing something
+            data.setData(2,
+                         random.randint(23, 25),
+                         True,
+                         False,
+                         random.randint(1, 3),
+                         False,
+                         False,
+                         random.random() < 0.5,
+                         random.random(),
+                         random.randint(80, 81),
+                         2,
+                         True,
+                         True,
+                         random.randint(30, 35),
+                         False,
+                         False)
 
-        elif int(play_time) == TIME_FEATURE_2:
+        elif int(play_time) >= TIME_FEATURE_2 and int(play_time) < TIME_FEATURE_3:
             print("feature2")
             # doing something
+            data.setData(3,
+                         random.randint(23, 25),
+                         True,
+                         False,
+                         random.randint(1, 3),
+                         True,
+                         False,
+                         random.random() < 0.5,
+                         random.random(),
+                         random.randint(80, 81),
+                         0,
+                         False,
+                         False,
+                         0,
+                         False,
+                         True)
 
-        elif int(play_time) == TIME_FEATURE_3:
+        elif int(play_time) >= TIME_FEATURE_3 and int(play_time) < 85:
             print("feature3")
             # doing something
+            data.setData(4,
+                         random.randint(23, 25),
+                         True,
+                         False,
+                         random.randint(1, 3),
+                         False,
+                         False,
+                         random.random() < 0.5,
+                         random.random(),
+                         random.randint(80, 81),
+                         2,
+                         True,
+                         True,
+                         0,
+                         False,
+                         False)
 
-        elif int(play_time) == TIME_FEATURE_4:
+        elif int(play_time) >= TIME_FEATURE_4 and int(play_time) < 100:
             print("feature4")
             # doing something
-        else:
+            data.setData(5,
+                         random.randint(23, 25),
+                         True,
+                         False,
+                         random.randint(1, 3),
+                         False,
+                         False,
+                         random.random() < 0.5,
+                         random.random(),
+                         random.randint(80, 81),
+                         0,
+                         False,
+                         False,
+                         random.randint(20, 25),
+                         True,
+                         False)
+        elif int(play_time) < TIME_FEATURE_1:
             # generate random data
             data.setData(0,
-                         random.randint(20, 25),
+                         33,
                          True,
                          False,
                          random.randint(1, 3),
@@ -115,37 +195,28 @@ def monitor_time():
                          random.randint(30, 35),
                          False,
                          False)
-            logger.set_logger(data)
-
-
-
-def show_logger():
-    # global logger
-    print("Thread3")
-    # logger.show_logger()
-
-
-def print_logger():
-    print("Thread4")
-    # while True:
-    #     try:
-    #         time.sleep(1)  # 暂停一秒
-    #         queue.put("新的信息\n")
-    #         if queue.empty():
-    #             pass
-    #         else:
-    #             logger.print_logger(queue.get())
-    #     except queue.Full:
-    #         pass
-
+        else:
+            data.setData(6,
+                         33,
+                         True,
+                         False,
+                         random.randint(1, 3),
+                         False,
+                         False,
+                         random.random() < 0.5,
+                         random.random(),
+                         random.randint(80, 81),
+                         0,
+                         True,
+                         True,
+                         random.randint(30, 35),
+                         False,
+                         False)
+        logger.set_logger(data)
 
 def run():
     t1 = threading.Thread(target=monitor_time)
-    t2 = threading.Thread(target=show_logger)
-    t3 = threading.Thread(target=print_logger)
     t1.start()
-    t2.start()
-    t3.start()
     play_video()
 
 if __name__ == '__main__':
